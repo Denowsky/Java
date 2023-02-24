dddd6d447082152ee31ccc87ad86182517a54f35
Denowsky <den.san404@gmail.com>
2023-02-24 19:46:11 +0300

    Сделана сортировка

--- Seminars/fourth/HW.java
+++ Seminars/fourth/HW.java
@@ -1,5 +1,9 @@
 package Seminars.fourth;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class HW {
@@ -12,31 +16,48 @@ public class HW {
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите Фамилию, имя, отчество, пол и возраст: ");
-
+        Integer count = 0;
         boolean run = true;
-        while (run){
+        while (run) {
             String in = scan.nextLine();
-            if (in.equals("x")){
+            if (in.equals("x")) {
                 run = false;
-            }
-            else{
+            } else {
                 String[] data = in.split(" ");
                 lastname.add(data[0]);
                 firstname.add(data[1]);
                 patronymic.add(data[2]);
                 sex.add(data[3]);
                 age.add(Integer.parseInt(data[4]));
+                count++;
+                continue;
             }
-
-        for(int i = 0; i<lastname.size();i++){
-            StringBuilder str = new StringBuilder();
-            str.append(lastname.get(i)).append(" ")
-                .append(firstname.get(i).charAt(0)).append(" ")
-                .append(patronymic(i).charAt(0)).append(" ")
-                .append(sex(i).charAt(0)).append(" ").append(age.get(i));
-            sout(str);
         }
 
+        System.out.println("Отсортировать список по возрасту? 'y' или 'n':");
+        String out = scan.nextLine();
+        scan.close();
+        ArrayList<Integer> sorted = age;
+        sorted.sort(null);
+        if (out.equals("y")) {
+            for (int i = 0; i < sorted.size(); i++) {
+                Integer j = age.indexOf(sorted.get(i));
+                StringBuilder str = new StringBuilder();
+                str.append(lastname.get(j)).append(" ")
+                        .append(firstname.get(j).charAt(0)).append(".")
+                        .append(patronymic.get(j).charAt(0)).append(". ")
+                        .append(age.get(j)).append(sex.get(j).charAt(0));
+                System.out.println(str);
+            }
+        } else {
+            for (int i = 0; i < sorted.size(); i++) {
+                StringBuilder str = new StringBuilder();
+                str.append(lastname.get(i)).append(" ")
+                        .append(firstname.get(i).charAt(0)).append(".")
+                        .append(patronymic.get(i).charAt(0)).append(". ")
+                        .append(age.get(i)).append(sex.get(i).charAt(0));
+                System.out.println(str);
+            }
         }
     }
 }
