95be4d4eea928a10026e23ac650e91fe0e495a1d
Denowsky <den.san404@gmail.com>
2023-02-28 22:03:32 +0300

    Добавлен 5й семинар

---
+++ Seminars/fifth/HW.java
@@ -0,0 +1,25 @@
+package Seminars.fifth;
+
+import java.security.Key;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HW {
+    public static void main(String[] args) {
+        Map<Integer,String> db = new HashMap<>();
+        db.put(1, "Значение 1");
+        db.put(2, "Значение 2");
+        db.put(3, "Значение 3");
+        db.put(4, "Значение 4");
+        db.put(5, "Значение 5");
+        db.put(6, "Значение 6");
+        // System.out.println(db.values());
+        for(Integer s:db.keySet()){
+            System.out.println(db.get(s));
+        }
+        for(int i=1; i<db.size()+1;i++){
+            db.compute(i, (k,v)-> v += "!");
+        }
+        System.out.println(db.values());
+    }
+}
